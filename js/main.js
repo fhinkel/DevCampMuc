56a5dfc79a4038ef1f99aeda571443a629131550
Raphael Boidol <raphael.boidol@tngtech.com>
2014-05-25 10:37:09 +0200

    update reveal, move closer to reveal structure, removed additional & unused css files

--- js/main.js
+++ js/main.js
@@ -53,9 +53,7 @@ require(['head', 'jquery', 'marked', 'custom/structure.builder', 'custom/issue.f
                 // Full list of configuration options available here:
                 // https://github.com/hakimel/reveal.js#configuration
                 Reveal.initialize({
-                    width: 1024,
-                    height: 768,
-                    controls: false,
+                    controls: true,
                     progress: true,
                     history: true,
                     center: true,
@@ -63,6 +61,10 @@ require(['head', 'jquery', 'marked', 'custom/structure.builder', 'custom/issue.f
 
                     theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
                     transition: Reveal.getQueryHash().transition || 'linear', // default/cube/page/concave/zoom/linear/fade/none
+
+                    // Parallax scrolling
+                    // parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
+                    // parallaxBackgroundSize: '2100px 900px',
 
                     // Optional libraries used to extend on reveal.js
                     dependencies: [
